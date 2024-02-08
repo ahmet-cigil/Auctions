c1cd3b7261031ab8addd851bf8c2282b3d748e1a
ahmet-cigil <ahmet.cigil@tuta.com>
2024-02-08 23:39:40 +0100

    Listing completed

--- Auctions/Models/Listing.cs
+++ Auctions/Models/Listing.cs
@@ -3,9 +3,28 @@ namespace Auctions.Models
 {
 	public class Listing
 	{
-		public Listing()
-		{
-		}
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		public double Price { get; set; }
+
+		public string ImagePath { get; set; }
+
+		public bool IsSold { get; set; }
+
+		[Required]
+
+		public string IdentityUserId? { get; set; }
+		[Foreignkey("IdentityUserId")]
+
+		public IdentityUser? User { get; set; }
+
+		public List <Bid>? Bids { get; set; }
+
+		public List <Comment>? Comments { get; set; }
 	}
 }
 
